435127ee87d27c9390c67d6fe3752a8ff30be51a
wangke <wangke_0809@126.com>
2020-06-21 16:22:58 +0800

    add test

--- src/test/java/org/stellar/sdk/RealServerTest.java
+++ src/test/java/org/stellar/sdk/RealServerTest.java
@@ -1,6 +1,8 @@
 package org.stellar.sdk;
 
 import org.junit.Test;
+import org.stellar.sdk.responses.AccountResponse;
+import org.stellar.sdk.responses.SubmitTransactionResponse;
 import org.stellar.sdk.responses.TransactionResponse;
 import org.stellar.sdk.xdr.TransactionEnvelope;
 import org.stellar.sdk.xdr.XdrDataInputStream;
@@ -8,7 +10,6 @@ import org.stellar.sdk.xdr.XdrDataInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Base64;
 
 /**
  * @author wangke
@@ -17,24 +18,28 @@ import java.util.Base64;
  */
 public class RealServerTest {
 
-    Server server = new Server("https://gatewayohio.bitrenren.com/xlm_node/");
+    Server server = new Server("https://horizon-testnet.stellar.org");
 
     @Test
-    public void testGetTx() throws Exception {
-        // payment
-        String txHash = "6cfe93a9194e57164b9d3a4904d715b4b9f8a2c00de2fdb87f30e5b7839b69ea";
-        final TransactionResponse tx = server.transactions().transaction(txHash);
-        System.out.println(tx.getEnvelopeXdr());
-        final byte[] bytes = Base64.getDecoder().decode(tx.getEnvelopeXdr());
-//        final byte[] bytes = Base64.decodeBase64(tx.getEnvelopeXdr());
-        InputStream inputStream = new ByteArrayInputStream(bytes);
-        TransactionEnvelope transactionEnvelope = null;
-        try {
-            transactionEnvelope = TransactionEnvelope.decode(new XdrDataInputStream(inputStream));
-            System.out.println(transactionEnvelope);
-        } catch (IOException e) {
-            System.out.println("!!!ERROR : " + e);
-        }
+    public void testSend() throws Exception{
+        final KeyPair keyPair = KeyPair.fromSecretSeed("SAR34YC3PRNKJB5B7VR4OVCD4A2KQBNVZS54J6TLK5IUDUEKVXFUBKJHKE");
+        String toAddress = "GCXLMSCTLEYM6X5NTBQZFF65EKJ4WMHROUL6K6IP4I3NCUCM764IFNIF";
+        final String accountId = keyPair.getAccountId();
+        System.out.println(accountId);
+
+        final AccountResponse account = server.accounts().account(keyPair.getAccountId());
+
+        Transaction.Builder builder = new Transaction.Builder(account, Network.TESTNET);
+
+        Transaction transaction = builder.setBaseFee(100)
+                .addOperation(new PaymentOperation.Builder(toAddress, new AssetTypeNative(), "1").build())
+                .addMemo(Memo.text("HI")).setTimeout(3600).build();
+        transaction.sign(keyPair);
+        final SubmitTransactionResponse submitTransactionResponse = server.submitTransaction(transaction);
+        final String hash = submitTransactionResponse.getHash();
+        System.out.println(hash);
+
     }
+
 
 }
